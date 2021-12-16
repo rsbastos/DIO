28dc07010f132f28353e6877197cbc2471050734
René Bastos <67017484+rsbastos@users.noreply.github.com>
2021-12-16 08:51:27 -0300

    Delete cities-api directory

--- cities-api/src/main/java/com/rsb/citiesapi/repository/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rsb.citiesapi.repository;
-
-import com.rsb.citiesapi.countries.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
