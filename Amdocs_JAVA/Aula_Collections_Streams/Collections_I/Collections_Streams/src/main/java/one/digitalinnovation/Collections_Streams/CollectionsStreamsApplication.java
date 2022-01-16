6d56fa66861d43a7b0871266a4ecd051e10a124e
René Bastos <67017484+rsbastos@users.noreply.github.com>
2022-01-16 17:33:19 -0300

    Add files via upload

---
+++ Amdocs_JAVA/Aula_Collections_Streams/Collections_I/Collections_Streams/src/main/java/one/digitalinnovation/Collections_Streams/CollectionsStreamsApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.Collections_Streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsStreamsApplication.class, args);
+	}
+
+}
---
+++ Amdocs_JAVA/Aula_Collections_Streams/Collections_I/Collections_Streams/src/test/java/one/digitalinnovation/Collections_Streams/CollectionsStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.Collections_Streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
