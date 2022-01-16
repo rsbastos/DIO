e1e7511aaa0eb480565e42b672b1b00026ab509a
René Bastos <67017484+rsbastos@users.noreply.github.com>
2022-01-16 17:10:10 -0300

    Add files via upload

---
+++ Amdocs_JAVA/Aula_Collections_Streams/Collections_I/collections_streams/src/test/java/one/digitalinnovation/collections_streams/CollectionsStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.collections_streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
