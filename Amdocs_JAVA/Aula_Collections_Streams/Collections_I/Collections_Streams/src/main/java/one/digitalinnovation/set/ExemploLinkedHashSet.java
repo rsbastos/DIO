73efb7cd48ec3b91fa2b90c085b8b327f9785c14
rsbastos <rene.s.bastos@gmail.com>
2022-01-18 09:15:34 -0300

    Criação da classe ExemploLinkedHashSet

---
+++ Amdocs_JAVA/Aula_Collections_Streams/Collections_I/Collections_Streams/src/main/java/one/digitalinnovation/set/ExemploLinkedHashSet.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.set;
+
+import java.util.LinkedHashSet;
+
+public class ExemploLinkedHashSet {
+
+    public static void main(String[] args) {
+
+        LinkedHashSet<Integer> sequenciaNumerica = new LinkedHashSet<>();
+
+        // Adiciona os números no set
+        sequenciaNumerica.add(1);
+        sequenciaNumerica.add(2);
+        sequenciaNumerica.add(4);
+        sequenciaNumerica.add(8);
+        sequenciaNumerica.add(16);
+
+        System.out.println("Sequência numérica: " + sequenciaNumerica);
+    }
+}
