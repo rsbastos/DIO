74cf622347c99c4cf02255154002769e6c4cec13
rsbastos <rene.s.bastos@gmail.com>
2022-01-18 19:44:11 -0300

    Finalização do comparators

--- Amdocs_JAVA/Aula_Collections_Streams/Collections_I/Collections_Streams/src/main/java/one/digitalinnovation/comparators/ComparatorsExampleList.java
+++ Amdocs_JAVA/Aula_Collections_Streams/Collections_I/Collections_Streams/src/main/java/one/digitalinnovation/comparators/ComparatorsExampleList.java
@@ -1,7 +1,6 @@
 package one.digitalinnovation.comparators;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ComparatorsExampleList {
 
@@ -9,6 +8,45 @@ public class ComparatorsExampleList {
 
         List<Estudante> estudantes = new ArrayList<>();
 
+        estudantes.add(new Estudante("Pedro", 19));
+        estudantes.add(new Estudante("Carlos", 23));
+        estudantes.add(new Estudante("Mariana", 21));
+        estudantes.add(new Estudante("João", 18));
+        estudantes.add(new Estudante("Thiago", 20));
+        estudantes.add(new Estudante("George", 22));
+        estudantes.add(new Estudante("Larissa", 21));
 
+        System.out.println("--- Ordem de Inserção ---");
+        System.out.println(estudantes);
+
+        estudantes.sort((first, second) -> first.getIdade() - second.getIdade());
+
+        System.out.println("--- Ordem Natural dos Números - Idade ---");
+        System.out.println(estudantes);
+
+        estudantes.sort((first, second) -> second.getIdade() - first.getIdade());
+
+        System.out.println("--- Ordem Reversa dos Números - Idade ---");
+        System.out.println(estudantes);
+
+        estudantes.sort(Comparator.comparingInt(Estudante::getIdade));
+
+        System.out.println("--- Ordem Natural dos Números - Idade (method reference) ---");
+        System.out.println(estudantes);
+
+        estudantes.sort(Comparator.comparingInt(Estudante::getIdade).reversed());
+
+        System.out.println("--- Ordem Reversa dos Números - Idade (method reference) ---");
+        System.out.println(estudantes);
+
+        Collections.sort(estudantes);
+
+        System.out.println("Ordem Natural dos Números - Idade (interface Comparable) ---");
+        System.out.println(estudantes);
+
+        Collections.sort(estudantes, new EstudanteOrdemIdadeReversaComparator());
+
+        System.out.println("--- Ordem Reversa dos Números - Idade (interface Comparator) --- ");
+        System.out.println(estudantes);
     }
 }
