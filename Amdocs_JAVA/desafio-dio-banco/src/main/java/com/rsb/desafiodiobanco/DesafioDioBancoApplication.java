778a5fa81e1e6aeaf53a366767f98df1f60221a4
René Bastos <67017484+rsbastos@users.noreply.github.com>
2022-02-01 10:09:12 -0300

    criação do repositório

---
+++ Amdocs_JAVA/desafio-dio-banco/src/main/java/com/rsb/desafiodiobanco/DesafioDioBancoApplication.java
@@ -0,0 +1,13 @@
+package com.rsb.desafiodiobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioBancoApplication.class, args);
+	}
+
+}
