79ec092714a5d56f375724ef64f3d4d416d7750e
rsbastos <rene.s.bastos@gmail.com>
2022-02-19 13:50:27 -0300

    criação de objetos

--- GFT_Java/desafio-poo-dio/src/Main.java
+++ GFT_Java/desafio-poo-dio/src/Main.java
@@ -1,8 +1,28 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-        Curso curso = new Curso();
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso Java");
+        curso1.setCargaHoraria(80);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição do curso JS");
+        curso2.setCargaHoraria(40);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria em Java");
+        mentoria.setDescricao("Descrição da Mentoria em Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println("\n" + curso2);
+        System.out.println("\n" + mentoria);
     }
 }
