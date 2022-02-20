4016c9273a25edf748b57a33920f7e1084805762
René Bastos <67017484+rsbastos@users.noreply.github.com>
2022-02-20 20:33:14 -0300

    Add files via upload

---
+++ GFT_Java/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/model/ClienteRepository.java
@@ -0,0 +1,6 @@
+package one.digitalinnovation.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+}
