785bedc98fc06f45046ccb37b7431586795eda52
rsbastos <rene.s.bastos@gmail.com>
2022-05-12 12:36:58 -0300

    build - App.js styles definitions

--- Carrefour_Web_Developer/covid19/src/App.js
+++ Carrefour_Web_Developer/covid19/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { StylesProvider } from '@material-ui/styles'  
+import { CssBaseline } from '@material-ui/core'
+import GlobalStyle from './commons/styles/global-style'
 
 function App() {
   return (
-    <div className="App">
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyle />
       
-    </div>
+    </StylesProvider>
   );
 }
 
