b7182c8ae4f821251a2b80038d0abd56bad8c5dc
rsbastos <rene.s.bastos@gmail.com>
2022-05-12 18:30:39 -0300

    fix - yarn - ESlint - package-lock

---
+++ Carrefour_Web_Developer/covid19/src/containers/Main/index.js
@@ -0,0 +1,13 @@
+import React, { memo } from 'react'
+import Api from '../../api'
+
+function Main() {
+
+    return (
+        <div>
+            Teste
+        </div>
+    )       
+}
+
+export default memo(Main)
