e1f6195b2f0ebb2e5822363eced1893be43dd500
rsbastos <rene.s.bastos@gmail.com>
2022-05-23 09:05:02 -0300

    fix - container component

--- Impulso_Fullstack_Web_Developer/dioshopping/src/App.js
+++ Impulso_Fullstack_Web_Developer/dioshopping/src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+import { Container } from '@material-ui/core';
 import Header from './components/Header';
 
 const App = () => { 
   return(
+    <Container maxWidth="xl">
+      <Router>
+        <Header />
+        <Routes />
+      </Router>      
+    </Container>
     
-    <Router>
-      <Header />
-      <Routes />
-    </Router>
   )
 }
  
--- Impulso_Fullstack_Web_Developer/dioshopping/src/Pages/home.js
+++ Impulso_Fullstack_Web_Developer/dioshopping/src/Pages/home.js
@@ -1,4 +1,4 @@
-import { Container, Paper, Grid, Typography, Button, List, ListItem, ListItemText, makeStyles } from '@material-ui/core';
+import { Paper, Grid, Typography, Button, List, ListItem, ListItemText, makeStyles } from '@material-ui/core';
 import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
@@ -16,103 +16,101 @@ const HomePage = () => {
     const classes = useStyles();
 
     return(
-        <Container maxWidth="xl">                      
-            <Grid container spacing={3} className={classes.root}>
-                <Grid item xs={3}>
-                    <Paper className={classes.paper}>
-                        <Typography variant='h5'>
-                            Categorias
-                        </Typography>
-                        <List>
-                            <ListItem>
-                                <ListItemText
+        <Grid container spacing={3} className={classes.root}>
+            <Grid item xs={3}>
+                <Paper className={classes.paper}>
+                    <Typography variant='h5'>
+                        Categorias
+                    </Typography>
+                    <List>
+                        <ListItem>
+                            <ListItemText
                                 primary="Times nacionais"
                                 secondary="3"     
-                                />                           
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
+                            />                           
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText
                                 primary="Times internacionais"
                                 secondary="3"     
                                 />                           
-                            </ListItem>
-                            <ListItem>
-                                <ListItemText
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText
                                 primary="Times históricos"
                                 secondary="3"     
                                 />                           
-                            </ListItem>
-                        </List>
-                    </Paper>
-                </Grid>
-                <Grid container xs={9} spacing={3} className={classes.root}>
-                    <Grid item xs="3">
-                        <Paper className={classes.paper}>
-                            <Grid container direction='column'>
-                                <Grid item>
-                                    <img width="140px" src="./images/produtos/fla.jpg" alt="Flamengo 2020"/>
-                                    <Typography variant='h6'>
-                                        Produto
-                                    </Typography>
-                                    <Typography variant='subtitled1'>
-                                        R$ 99.00
-                                    </Typography>
-                                </Grid>
-                            <Button variant="contained">Adicionar</Button>    
-                            </Grid>
-                        </Paper>
+                        </ListItem>
+                    </List>
+                </Paper>
+            </Grid>
+        <Grid container xs={9} spacing={3} className={classes.root}>
+            <Grid item xs="3">
+                <Paper className={classes.paper}>
+                    <Grid container direction='column'>
+                        <Grid item>
+                            <img width="140px" src="./images/produtos/fla.jpg" alt="Flamengo 2020"/>
+                            <Typography variant='h6'>
+                                Produto
+                            </Typography>
+                            <Typography variant='subtitled1'>
+                                 R$ 99.00
+                            </Typography>
+                        </Grid>
+                        <Button variant="contained">Adicionar</Button>    
                     </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <Grid container direction='column'>
-                                <Grid item>
-                                <img width="140px" src="./images/produtos/fla.jpg" alt="Flamengo 2020"/>
-                                    <Typography variant='h6'>
-                                        Produto
-                                    </Typography>
-                                    <Typography variant='subtitled1'>
-                                        R$ 99.00
-                                    </Typography>
-                                </Grid>
-                            <Button variant="contained">Adicionar</Button>    
-                            </Grid>
-                        </Paper>
+                </Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper className={classes.paper}>
+                    <Grid container direction='column'>
+                        <Grid item>
+                            <img width="140px" src="./images/produtos/fla.jpg" alt="Flamengo 2020"/>
+                            <Typography variant='h6'>
+                                Produto
+                            </Typography>
+                            <Typography variant='subtitled1'>
+                                R$ 99.00
+                            </Typography>
+                        </Grid>
+                        <Button variant="contained">Adicionar</Button>    
                     </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <Grid container direction='column'>
-                                <Grid item>
-                                <img width="140px" src="./images/produtos/fla.jpg" alt="Flamengo 2020"/>
-                                    <Typography variant='h6'>
-                                        Produto
-                                    </Typography>
-                                    <Typography variant='subtitled1'>
-                                        R$ 99.00
-                                    </Typography>
-                                </Grid>
-                            <Button variant="contained">Adicionar</Button>    
-                            </Grid>
-                        </Paper>
+                </Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper className={classes.paper}>
+                    <Grid container direction='column'>
+                        <Grid item>
+                            <img width="140px" src="./images/produtos/fla.jpg" alt="Flamengo 2020"/>
+                            <Typography variant='h6'>
+                                Produto
+                            </Typography>
+                            <Typography variant='subtitled1'>
+                                R$ 99.00
+                            </Typography>
+                        </Grid>
+                        <Button variant="contained">Adicionar</Button>    
                     </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <Grid container direction='column'>
-                                <Grid item>
-                                <img width="140px" src="./images/produtos/fla.jpg" alt="Flamengo 2020"/>
-                                    <Typography variant='h6'>
-                                        Produto
-                                    </Typography>
-                                    <Typography variant='subtitled1'>
-                                        R$ 99.00
-                                    </Typography>
-                                </Grid>
-                            <Button variant="contained">Adicionar</Button>    
-                            </Grid>
-                        </Paper>
+                </Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper className={classes.paper}>
+                    <Grid container direction='column'>
+                        <Grid item>
+                            <img width="140px" src="./images/produtos/fla.jpg" alt="Flamengo 2020"/>
+                            <Typography variant='h6'>
+                                Produto
+                            </Typography>
+                            <Typography variant='subtitled1'>
+                                R$ 99.00
+                            </Typography>
+                        </Grid>
+                        <Button variant="contained">Adicionar</Button>    
+                     </Grid>
+                </Paper>
                     </Grid>
-                </Grid>
-            </Grid>            
-        </Container>
+            </Grid>
+        </Grid> 
     )
 }
 
