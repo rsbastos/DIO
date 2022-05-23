5daad4bd529ea0567ed13df230d20d97c4fe6940
rsbastos <rene.s.bastos@gmail.com>
2022-05-22 21:01:33 -0300

    new push - wrong path fixed

---
+++ Impulso_Fullstack_Web_Developer/dioshopping/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import MainRoutes from './routes';
+
+const App = () => { 
+  return(
+    
+    <Router>
+      <MainRoutes />
+    </Router>
+  )
+}
+ 
+
+export default App;
