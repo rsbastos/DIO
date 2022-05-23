cf3a82a88a67fe8d03f4f320ad6ad691f3724c30
rsbastos <rene.s.bastos@gmail.com>
2022-05-23 16:18:44 -0300

    bug fix - back to createStore

--- Impulso_Fullstack_Web_Developer/dioshopping/src/components/store/index.js
+++ Impulso_Fullstack_Web_Developer/dioshopping/src/components/store/index.js
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore } from 'redux';
 import rootReducer from './reducers';
 
-const store = configureStore(rootReducer)
+const store = createStore(rootReducer)
 
 export default store;
