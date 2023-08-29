c855d88b3e3b05de306d9c0a9825977bbc04b21c
rsbastos <renedossantosbastos@gmail.com>
2023-08-29 18:46:59 -0300

    :recycle: refactor: using MAP on pokeApi

--- Santander/POKEDEX/assets/js/main.js
+++ Santander/POKEDEX/assets/js/main.js
@@ -1,8 +1,4 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -16,22 +12,13 @@ function convertPokemonToLi(pokemon) {
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
                     alt="${pokemon.name}">
             </div>    
-        </li> `
-    
+        </li> `    
 }
 
 const pokemonList = document.getElementById("pokemonList")  
 
-
-pokeApi.getPokemons().then((pokemons) => {        
-    const listItems = []       
-    
-    for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            listItems.push(convertPokemonToLi(pokemon))  
-        }
-
-    console.log(listItems)     
+pokeApi.getPokemons().then((pokemons = []) => {    
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')    
 })    
    
     
