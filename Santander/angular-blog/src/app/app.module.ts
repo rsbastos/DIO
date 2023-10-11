7265e08ac5c31c54df30475a7f62e18479e4b9dc
rsbastos <renedossantosbastos@gmail.com>
2023-10-11 17:25:43 -0300

    files structure

--- Santander/angular-blog/src/app/app.module.ts
+++ Santander/angular-blog/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,  
+    BigCardComponent,
+    SmallCardComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule
