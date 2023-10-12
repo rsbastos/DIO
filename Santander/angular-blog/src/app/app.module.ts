8ee5561dd9a6fa5a46ac253b37cfa897ffdc376c
rsbastos <renedossantosbastos@gmail.com>
2023-10-12 12:26:48 -0300

    big card structure layout

--- Santander/angular-blog/src/app/app.module.ts
+++ Santander/angular-blog/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MenuTitleComponent } from './components/menu-title/menu-title.component
     MenuBarComponent,  
     BigCardComponent,
     SmallCardComponent,
-    MenuTitleComponent
+    MenuTitleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
